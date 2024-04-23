c1111d63a264652da6997fc3f5f3827854f04e27
Ashutosh aditya <adityaashutosh6@gmail.com>
2024-04-24 00:44:50 +0530

    added unit features in cart

--- src/redux/cartSlice.jsx
+++ src/redux/cartSlice.jsx
@@ -1,45 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { toast } from 'react-toastify'
+// import { createSlice } from '@reduxjs/toolkit'
+// import { toast } from 'react-toastify'
 
-const initialState = JSON.parse(localStorage.getItem('cart')) ?? [];
+// const initialState = JSON.parse(localStorage.getItem('cart')) ?? [];
 
 // const cartSlice = createSlice({
 //     name: 'cart',
 //     initialState,
 //     reducers: {
 //         addToCart(state, action) {
-//             state.push(action.payload)
+//             state.push(action.payload);
 //         },
-//         // deleteFromCart(state, action) {
-//         //     toast.success("Function Called");
-//         //     return state.filter(item => item.id != action.payload);
-//         // }
 //         deleteFromCart(state, action) {
 //             const itemIdToRemove = action.payload;
-//             toast.success('Function Called');
-//             return state.filter(item => item.id !== itemIdToRemove);
-//         }   
+//             // console.log('Item ID to remove:', itemIdToRemove); // Log the ID being removed
+//             const newState = state.filter(item => item.id !== itemIdToRemove);
+//             // console.log('New state after deletion:', newState); // Log the updated state
+//             return newState;
+//         }
 //     }
-// })
-// cartSlice.jsx
+// });
+
+
+// export const { addToCart, deleteFromCart } = cartSlice.actions
+
+// export default cartSlice.reducer;
+
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify'
+
+const initialState = JSON.parse(localStorage.getItem('cart')) ?? [];
+
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addToCart(state, action) {
-            state.push(action.payload);
-        },
-        deleteFromCart(state, action) {
-            const itemIdToRemove = action.payload;
-            // console.log('Item ID to remove:', itemIdToRemove); // Log the ID being removed
-            const newState = state.filter(item => item.id !== itemIdToRemove);
-            // console.log('New state after deletion:', newState); // Log the updated state
-            return newState;
-        }
-    }
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      state.push(action.payload);
+    },
+    deleteFromCart(state, action) {
+      const itemIdToRemove = action.payload;
+      return state.filter((item) => item.id !== itemIdToRemove);
+    },
+    updateCartItemQuantity(state, action) {
+      return action.payload; // Update the entire cart state with new quantities
+    },
+  },
 });
 
-
-export const { addToCart, deleteFromCart } = cartSlice.actions
+export const { addToCart, deleteFromCart, updateCartItemQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer;
